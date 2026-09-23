c35fd6553dad211336c5672ed131aa2ba6738d8b
EssEnemiGz <75174446+EssEnemiGz@users.noreply.github.com>
2026-09-22 22:52:20 -0400

    Added valid diccionary words and common words for random challengue

--- main.py
+++ main.py
@@ -5,19 +5,32 @@ By: EssEnemiGz
 Color code:
 
 RED (31): NOT IN
-YELLOW (33): WRONG POSITION
 GREEN (32): CORRECT
+YELLOW (33): WRONG POSITION
 """
 
-from typing import List
+from random import choice
+import json
 
-word = "HELLO"
 RESET_COLOR = "\033[0m"
 RED = "\033[31m"
 YELLOW = "\033[33m"
 GREEN = "\033[32m"
 
-def comprobation(user_input: str):
+with open("all_words.json", "r") as aw:
+    all_words = json.load(aw).get("words")
+    all_words = set(all_words)
+
+with open("words.json", "r") as w:
+    words = json.load(w).get("words")
+    words = set(words)
+    max_idx = len(words)-1
+
+def random_word() -> str:
+    random_word = choice(tuple(words))
+    return random_word
+
+def comprobation(user_input: str, word: str) -> str:
     colors = [RED] * len(word)
     letters = list(word)
 
@@ -35,10 +48,12 @@ def comprobation(user_input: str):
     for idx in range(len(word)):
         output += f"{colors[idx]}{user_input[idx]}{RESET_COLOR}"
 
-    print(output)
+    return output
 
-def main():
+def main() -> None:
     attempts = 1
+    word = random_word()
+    word = word.upper()
     while attempts <= 6:
         answer = input("ANSWER: ")
         answer = answer.upper()
@@ -50,7 +65,12 @@ def main():
             print(f"{RED}ANSWER MUST BE 5 CHARACTERS!{RESET_COLOR}")
             continue
 
-        comprobation(answer)
+        if answer not in all_words:
+            print(f"{RED}NOT VALID WORD{RESET_COLOR}")
+            continue
+
+        comprobation_o = comprobation(answer, word)
+        print(comprobation_o)
         print(f"{YELLOW}ATTEMPT: {attempts}{RESET_COLOR}")
 
         if answer == word:
